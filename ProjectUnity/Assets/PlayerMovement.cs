d79bea10ac87000b6723a87fb2a8f5b0c5a5007f
AdamDWalker <adamwalker03@googlemail.com>
2015-10-26 21:03:34 +0000

    Player can move with WASD or arrow keys
    Also it doesn’t fall over now, which is good

---
+++ ProjectUnity/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+	public int speed = 5;
+	Rigidbody playerRigidbody;
+	Vector3 Velocity;
+
+	// Use this for initialization
+	void Start () {
+
+		playerRigidbody = GetComponent<Rigidbody> ();
+
+	}
+
+	void Update() {
+	
+		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+		Velocity = moveInput.normalized * speed;
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		playerRigidbody.MovePosition (playerRigidbody.position + Velocity * Time.fixedDeltaTime);
+	}
+}
