989674410dc00fa7eca2875eaf3993c95086752c
nevster08 <nevster07@gmail.com>
2015-10-28 22:46:53 +0000

    Made the enemies follow the player

---
+++ ProjectUnity/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player"); // find the player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mySpeed = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, mySpeed); // move the enemy
+    }
+}
