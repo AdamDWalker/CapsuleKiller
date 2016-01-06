e1a489d04c18289745d06c023a29826ef4c45be3
AdamDWalker <adamwalker03@googlemail.com>
2016-01-06 19:32:15 +0000

    Bullet spawns on mouse press

---
+++ ProjectUnity/Assets/Scripts/FireGun.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireGun : MonoBehaviour
+{
+
+    Vector3 bulletSpawn;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start ()
+    {
+        bulletSpawn = transform.Find("BulletSpawn").position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ( Input.GetMouseButtonDown(0) )
+        {
+            GameObject newBullet = new GameObject();
+            newBullet.transform.parent = gameObject.transform;
+            newBullet.transform.position = bulletSpawn;
+            Instantiate(newBullet);
+        }
+	}
+}
