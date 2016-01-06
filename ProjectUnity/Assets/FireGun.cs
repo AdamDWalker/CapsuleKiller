afa18d95f47d5b31178fedbf8b388df3c12b0822
AdamDWalker <adamwalker03@googlemail.com>
2016-01-06 18:53:00 +0000

    Added bullet to the scene but not done anything with it yet

---
+++ ProjectUnity/Assets/FireGun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireGun : MonoBehaviour
+{
+
+    Vector3 bulletSpawn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        bulletSpawn = transform.Find("BulletSpawn").position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
