4645c4df5aa22b527b601de954cbd38cb0ccfaaf
nevster08 <nevster07@gmail.com>
2016-01-06 21:24:13 +0000

    Raycast to set the player rotation

--- ProjectUnity/Assets/Scripts/PlayerMovement.cs
+++ ProjectUnity/Assets/Scripts/PlayerMovement.cs
@@ -20,17 +20,25 @@ public class PlayerMovement : MonoBehaviour {
 		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
 		Velocity = moveInput.normalized * speed;
 
-        // look at mouse
-        Vector3 MousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
-        Vector3 LookPos = Camera.main.ScreenToWorldPoint(MousePos);
-
-        transform.LookAt(LookPos);
-
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
 
 		playerRigidbody.MovePosition (playerRigidbody.position + Velocity * Time.fixedDeltaTime);
-	}
+
+        // look at mouse
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        float camDistance = 100;
+
+        Debug.DrawRay(camRay.origin, camRay.direction*camDistance, Color.green);
+
+        Vector3 endPoint = camRay.origin + (camRay.direction * camDistance);
+        endPoint.y = 0;
+
+        Quaternion newRotation = Quaternion.LookRotation(endPoint);
+
+        playerRigidbody.MoveRotation(newRotation);
+    }
 }
