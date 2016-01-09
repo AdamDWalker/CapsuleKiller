585912b145083d91e06fa479859914056321b4b0
nevster08 <nevster07@gmail.com>
2016-01-09 18:00:25 +0000

    Changed the raycast to use a plane
    The player still does not look at the correct point though.

--- ProjectUnity/Assets/Scripts/PlayerMovement.cs
+++ ProjectUnity/Assets/Scripts/PlayerMovement.cs
@@ -30,15 +30,24 @@ public class PlayerMovement : MonoBehaviour {
         // look at mouse
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        float camDistance = 100;
+        float camDistance = 50;
 
         Debug.DrawRay(camRay.origin, camRay.direction*camDistance, Color.green);
 
-        Vector3 endPoint = camRay.origin + (camRay.direction * camDistance);
-        endPoint.y = 0;
+        Plane hPlane = new Plane(Vector3.up, Vector3.zero);
+        // Plane.Raycast stores the distance from ray.origin to the hit point in this variable:
+        float distance = 0;
+        // if the ray hits the plane...
+        if (hPlane.Raycast(camRay, out distance))
+        {
+            // get the hit point:
+            Vector3 hitPoint = camRay.GetPoint(distance);
 
-        Quaternion newRotation = Quaternion.LookRotation(endPoint);
+            Debug.Log("Hit point: " + hitPoint);
 
-        playerRigidbody.MoveRotation(newRotation);
+            Quaternion newRotation = Quaternion.LookRotation(hitPoint);
+
+            playerRigidbody.MoveRotation(newRotation);
+        }
     }
 }
