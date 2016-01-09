c87eea75890de39075cc23941bb51c6734c624df
nevster08 <nevster07@gmail.com>
2016-01-09 18:15:22 +0000

    Added comments, turned enemies back on

--- ProjectUnity/Assets/Scripts/PlayerMovement.cs
+++ ProjectUnity/Assets/Scripts/PlayerMovement.cs
@@ -27,24 +27,26 @@ public class PlayerMovement : MonoBehaviour {
 
 		playerRigidbody.MovePosition (playerRigidbody.position + Velocity * Time.fixedDeltaTime);
 
-        // look at mouse
+        // create a ray from the camera to the to where the mouse is
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         float camDistance = 50;
 
+        // debug raycast (viewable in scene view)
         Debug.DrawRay(camRay.origin, camRay.direction*camDistance, Color.green);
 
+        // create a plane to detect the collision 
         Plane hPlane = new Plane(Vector3.up, Vector3.zero);
-        // Plane.Raycast stores the distance from ray.origin to the hit point in this variable:
+
+        // Plane.Raycast stores the distance from ray.origin to the hit point in this variable
         float distance = 0;
         // if the ray hits the plane...
         if (hPlane.Raycast(camRay, out distance))
         {
-            // get the hit point:
+            // get the hit point
             Vector3 hitPoint = camRay.GetPoint(distance);
 
-            Debug.Log("Hit point: " + hitPoint);
-
+            // make the player look at the point
             transform.LookAt(new Vector3(hitPoint.x, transform.position.y, hitPoint.z));
         }
     }
