4422f6a8c57a2f8d8fdac4767d3cd0c212f4c56d
Neville King <nevster07@gmail.com>
2016-01-13 17:27:39 +0000

    Added score script
    Score now works but nothing calls it yet.

---
+++ ProjectUnity/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+    private static double score;
+
+    /// <summary>
+    /// Get the score
+    /// </summary>
+    /// <returns>The score value</returns>
+	public static double getScore()
+    {
+        return score;
+    }
+
+    /// <summary>
+    /// Increases the score by a certain value
+    /// </summary>
+    /// <param name="scoreValue">Score to increase by</param>
+    public static void increaseScore(double scoreValue)
+    {
+        score += scoreValue;
+        Debug.Log("Score increased. Score is now" + score);
+    }
+}
