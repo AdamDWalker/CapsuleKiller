683725b957f2f31e213e4edfc1061be4408d0be4
Neville King <nevster07@gmail.com>
2016-01-21 14:56:43 +0000

    Player now spawns in a random place

--- ProjectUnity/Assets/Scripts/playerSpawn.cs
+++ ProjectUnity/Assets/Scripts/playerSpawn.cs
@@ -3,8 +3,19 @@ using System.Collections;
 
 public class playerSpawn : MonoBehaviour {
 
+    private Floor floor;
+
 	// Use this for initialization
 	void Start () {
-	    
+        floor = GameObject.Find("Floor").GetComponent<Floor>();
+
+        // spawn the player in a random postion
+        GridNode n;
+        do
+        {
+            n = floor.getBlockSpawn();
+        } while (!n.isWalkable()); // keep finding a different block until it is walkable
+
+        transform.position = n.getCentrePosition();
 	}
 }
